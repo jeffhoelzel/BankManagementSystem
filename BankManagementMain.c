d0e1712200dbb291be5e97afc707e212636a2bda
Jeffrey Hoelzel <jeffreyhoelzeljr@gmail.com>
2023-12-18 12:34:13 -0700

    Adding C library files

---
+++ BankManagementMain.c
@@ -0,0 +1,100 @@
+// header files
+#include "lib/CreateAccount_lib.h"
+#include "lib/Login_lib.h"
+#include "lib/TransferCash_lib.h"
+#include "lib/AccountInteraction_lib.h"
+
+// main program
+int main()
+{
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	// return success
+	return 0;
+}
