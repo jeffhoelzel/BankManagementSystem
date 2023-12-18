c614db875b47f46feebe7bfbc8a5ed41b8bddbb6
Jeffrey Hoelzel <jeffreyhoelzeljr@gmail.com>
2023-12-18 13:54:44 -0700

    Removed TransferCash_lib.h file and header in main

--- BankManagementMain.c
+++ BankManagementMain.c
@@ -1,12 +1,12 @@
 // header files
 #include "lib/CreateAccount_lib.h"
 #include "lib/Login_lib.h"
-#include "lib/TransferCash_lib.h"
 #include "lib/AccountInteraction_lib.h"
 
 // main program
 int main()
 {
+
 
 
 
