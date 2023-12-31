c3a2f7afacd904bef76e8d80f8ac1b95dd5fb405
Jeffrey Hoelzel <jeffreyhoelzeljr@gmail.com>
2023-12-30 17:48:40 -0700

    Added long-form descriptions for each function

--- AccountInteraction_lib.h
+++ AccountInteraction_lib.h
@@ -10,7 +10,9 @@
  * @brief Allow the user to tranfer money from their current account to another 
  * 		  account given that account's username.
  * 
- * *INSERT LONGER DESCRIPTION*
+ * Given a pointer to the user's account, prompt them for a username and dollar amount to 
+ * transfer money from their current account to this other account, and then print a receipt 
+ * for their transaction.
  * 
  * @param userAccount: a pointer to the user's account. 
  * @return void
@@ -20,18 +22,21 @@ void transferMoney(Account* userAccount);
 /**
  * @brief Allow the user to make a purchase.
  * 
- * *INSERT LONGER DESCRIPTION*
+ * Given a pointer to the user's account, the total cost of the item, and the name of the
+ * item itself, remove the cost of the item from the user's account and then print a receipt
+ * of the transaction including cost, sales tax, and the name of the item.
  * 
  * @param userAccount: a pointer to the user's account.
  * 		  cost: the cost of the purchase as a double.
+ * 		  item: the item the user is going to purchase as a string.
  * @return void
  */
-void purchaseItem(Account* userAccount, double cost);
+void purchaseItem(Account* userAccount, double cost, char item[64]);
 
 /**
  * @brief Allow the user to view the balance of their account.
  * 
- * *INSERT LONGER DESCRIPTION*
+ * Given a pointer to the user's account, print the balance for the user to view.
  * 
  * @param userAccount: a pointer to the user's account.
  * @return void
@@ -42,7 +47,9 @@ void viewBalance(Account* userAccount);
  * @brief Deletes an account after being provided with a username and 
  * 		  password. 
  * 
- * *INSERT LONGER DESCRIPTION*
+ * Given a pointer to the user's account, allow them to delete their account if the 
+ * username and password they provided is correct. Correctly restructure the linked list
+ * if neccessary and notify user if account deletion is successful.
  * 
  * @param void
  * @return void
@@ -53,7 +60,9 @@ void deleteAccount();
  * @brief Print a menu displaying all of the options the user has choose from 
  * 		  regarding interacting with their specific account.
  * 
- * *INSERT LONGER DESCRIPTION*
+ * Given the account type as a character, print a list specific to that type of account
+ * and allow the user to pick from it. Return an integer value representing which option the 
+ * user decided to choose.
  * 
  * @param accountType: a character representing the type of account the user owns.
  * @return choice: the user's choice picked from the menu
@@ -64,7 +73,8 @@ int getUserActionChoice(char accountType);
  * @brief User can choose what to do with their account based upon return value
  * 		  from the getUserActionChoice function.
  * 
- * *INSERT LONGER DESCRIPTION*
+ * Implement the entire system for user choice and execution based upon the integer value
+ * of their choice.
  * 
  * @param choice: an integer representing the user's choice from 
  * 				  getUserActionChoice.
