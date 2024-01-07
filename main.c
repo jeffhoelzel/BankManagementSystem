55a4cb1d54f4515703bee8d5e9e467bc01c9de00
Jeffrey Hoelzel <jeffreyhoelzeljr@gmail.com>
2024-01-06 23:24:33 -0700

    Added main.c

---
+++ main.c
@@ -0,0 +1,101 @@
+// header files
+#include "dtypes.h"
+#include "CreateAccount_lib.h"
+#include "Login_lib.h"
+#include "AccountInteraction_lib.h"
+
+// main program
+int main()
+{
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	// return success
+	return 0;
+}
